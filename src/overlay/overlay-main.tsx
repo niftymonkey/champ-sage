0b8116081cb9516bb1b8f420878e884af84f68d1
Mark Lozano <mark.d.lozano@gmail.com>
2026-05-04 13:39:20 -0600

    feat(ui): adopt v16 oxblood tokens and Fraunces/Inter/JetBrains Mono

--- src/overlay/overlay-main.tsx
+++ src/overlay/overlay-main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "../styles/tokens.css";
 import { OverlayApp } from "./OverlayApp";
 
 createRoot(document.getElementById("overlay-root")!).render(
